d9be93cc222eea4b1e7eab5af2c54aaa94050502
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-15 21:09:37 +0300

    Add images & text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-import './Home.module.scss'
+import classes from './Home.module.scss'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className={classes.homeContainer}>
+      <div className={classes.imageContainer}>
+        {/* <img
+          className={classes.image}
+          // TODO: Добавить нужную фотку
+          src="https://c.pxhere.com/photos/d0/f2/black_wallpaper_black_and_white_casual_dark_fashion_jacket_man_model-924824.jpg!d"
+          alt="man"
+        /> */}
+      </div>
+      <div className={classes.textContainer}>
+        <p className={classes.mainText}>Frontend</p>
+        <p className={classes.otherText}>developer</p>
+      </div>
+    </div>
+  )
 }
 
 export default Home
