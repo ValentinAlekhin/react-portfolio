9d2815af0f94c4fb08d19ee9b8597912992b957e
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-15 18:46:54 +0300

    Finish Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-import './Header.module.scss'
+import classes from './Header.module.scss'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className={'mainContainer'}>
+      <header className={classes.headerContainer}>
+        <div className={classes.nameContainer}>
+          {/* TODO: Добавить лого */}
+          <h1 className={classes.nameTitle}>
+            <Link to={'/'}>Valentin Alekhin</Link>
+          </h1>
+        </div>
+        <nav className={classes.navContainer}>
+          <ul className={classes.navList}>
+            <li className={classes.navItem}>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li className={classes.navItem}>
+              <Link to={'/projects'}>Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
 }
 
 export default Header
