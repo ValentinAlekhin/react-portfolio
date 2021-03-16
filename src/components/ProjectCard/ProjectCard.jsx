8bea16c9a5ac95614ac80221922430c47012eef6
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-16 15:56:12 +0300

    Add ProjectCard component

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import classes from './ProjectCard.module.scss'
+
+const ProjectCard = () => {
+  return <div className={classes.ProjectCard}>ProjectCard</div>
+}
+
+export default ProjectCard
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-import './Projects.module.scss'
+import classes from './Projects.module.scss'
+
+import ProjectCard from '../../components/ProjectCard/ProjectCard'
 
 const Projects = () => {
-  return <div>Projects</div>
+  return (
+    <div className="mainContainer">
+      <div className="contentContainer">
+        <div className={classes.Projects}>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Projects
