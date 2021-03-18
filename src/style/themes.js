9f716338b26789bbfd3b1ced07593bbcf42eabc5
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-18 21:42:13 +0300

    Add Nav Link style

--- src/style/themes.js
+++ src/style/themes.js
@@ -1,9 +1,14 @@
+import { darken, lighten } from 'polished'
+
 import darkBg from './pattern.svg'
 
 export const dark = {
   id: '0',
   background: darkBg,
-  fontColor: ' #fefefe',
+  fontColor: '#fefefe',
+  navLink: {
+    notActiveColor: darken(0.4, '#fefefe'),
+  },
   selection: {
     color: '#000',
     background: '#9b9b9b',
@@ -13,7 +18,10 @@ export const dark = {
 export const light = {
   id: '1',
   background: darkBg,
-  fontColor: ' black',
+  fontColor: '#000',
+  navLink: {
+    notActiveColor: lighten(0.4, '#fefefe'),
+  },
   selection: {
     color: '#fff',
     background: '#000',
