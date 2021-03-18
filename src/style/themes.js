8cb714a8d07adc2894a5aba03be364d081848514
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-18 20:55:30 +0300

    Add themes

---
+++ src/style/themes.js
@@ -0,0 +1,21 @@
+import darkBg from './pattern.svg'
+
+export const dark = {
+  id: '0',
+  background: darkBg,
+  fontColor: ' #fefefe',
+  selection: {
+    color: '#000',
+    background: '#9b9b9b',
+  },
+}
+
+export const light = {
+  id: '1',
+  background: darkBg,
+  fontColor: ' black',
+  selection: {
+    color: '#fff',
+    background: '#000',
+  },
+}
