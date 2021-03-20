3da1e81a0e76e928f88665cc4ca0ebf03a70fa6a
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-20 04:29:39 +0300

    Add mainContainer class

--- src/style/globalStyle.js
+++ src/style/globalStyle.js
@@ -1,5 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
+import { mainContainerWidthInPercentage as mainContainerWidth } from './_variables'
+import { respondTo } from './_respondTo'
+
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
@@ -30,6 +33,27 @@ const GlobalStyle = createGlobalStyle`
       background: ${({ theme }) => theme.selection.background};
       color: ${({ theme }) => theme.selection.color};
     }
+  }
+
+  .mainContainer {
+    width: ${mainContainerWidth.xs}%;
+    margin: 0 auto;
+
+    ${respondTo.sm`
+      width: ${mainContainerWidth.sm}%;
+    `}
+
+    ${respondTo.md`
+      width: ${mainContainerWidth.md}%;
+    `}
+
+    ${respondTo.lg`
+      width: ${mainContainerWidth.lg}%;
+    `}
+    
+    ${respondTo.xl`
+      width: ${mainContainerWidth.xl}%;
+    `}
   }
 `
 
