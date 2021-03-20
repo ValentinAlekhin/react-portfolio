e40f7f480cdb8bacb2dd9721501e77458d26a94b
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-20 17:33:11 +0300

    Add Local storage theme save

--- src/context/ThemeState.js
+++ src/context/ThemeState.js
@@ -6,17 +6,36 @@ import { dark, light } from '../style/themes'
 export const ThemeContext = createContext({
   theme: dark,
   toggleTheme: () => {},
+  setThemeFromLocalStorage: () => {},
 })
 
 const ThemeState = ({ children }) => {
   const [theme, setTheme] = useState(dark)
 
+  const setThemeFromLocalStorage = () => {
+    const themeId = localStorage.getItem('theme_id') || '0'
+    console.log(themeId)
+    themeId === '0' ? setTheme(dark) : setTheme(light)
+  }
+
+  const saveThemeToLocalStorage = themeId => {
+    localStorage.setItem('theme_id', themeId)
+  }
+
   const toggleTheme = () => {
-    theme.id === '0' ? setTheme(light) : setTheme(dark)
+    if (theme.id === '0') {
+      setTheme(light)
+      saveThemeToLocalStorage(light.id)
+    } else {
+      setTheme(dark)
+      saveThemeToLocalStorage(dark.id)
+    }
   }
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider
+      value={{ theme, toggleTheme, setThemeFromLocalStorage }}
+    >
       {children}
     </ThemeContext.Provider>
   )
