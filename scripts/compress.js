5a1408aba14591cc5c03a48129e4e3cf779e80c0
Alekhin Valentin <headbangernetwork@gmail.com>
2021-03-30 19:19:00 +0300

    Disable logs in compresss script

--- scripts/compress.js
+++ scripts/compress.js
@@ -3,16 +3,11 @@ const { Compress } = require('gzipper')
 const buildDir = './build'
 
 const gzip = new Compress(buildDir, null, { level: 9 })
-const brotli = new Compress(buildDir, null, { brotli: true, level: 11 })
+const brotli = new Compress(buildDir, null, { brotli: true, brotliQuality: 11 })
 
 const start = async () => {
   try {
-    const [gzipFiles, brotliFiles] = await Promise.all([
-      gzip.run(),
-      brotli.run(),
-    ])
-    console.info('Compressed gzip files: ', gzipFiles)
-    console.info('Compressed brotli files: ', brotliFiles)
+    await Promise.all([gzip.run(), brotli.run()])
   } catch (err) {
     console.error(err)
   }
